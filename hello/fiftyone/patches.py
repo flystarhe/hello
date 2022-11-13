282732df6c841481cf7f73c2094c774c8d595df6
hejian* <flystarhe@qq.com>
2022-11-13 10:13:56 +0800

    fix hello/fiftyone/patches.py

--- hello/fiftyone/patches.py
+++ hello/fiftyone/patches.py
@@ -1,11 +1,9 @@
 import json
 import shutil
 import time
-from collections import defaultdict
 from pathlib import Path
 
 import cv2 as cv
-import numpy as np
 from tqdm import tqdm
 
 
@@ -15,16 +13,18 @@ def extract_patch(out_dir, dataset, class_names, field_name="segmentations", pre
 
 
 def from_coco_instance(out_dir, dataset, class_names, field_name="segmentations", prefix=None):
-    _db = defaultdict(list)
-
     out_dir = Path(out_dir)
     shutil.rmtree(out_dir, ignore_errors=True)
     (out_dir / "data").mkdir(parents=True, exist_ok=False)
 
+    if class_names is not None:
+        class_names = set(class_names)
+
     if prefix is None:
         prefix = time.strftime(r"%y%m%d_%H%M%S")
 
-    class_names = set(class_names)
+    _db = {}
+    index = 0
     for sample in tqdm(dataset):
         img = cv.imread(sample.filepath, 1)
 
@@ -35,8 +35,10 @@ def from_coco_instance(out_dir, dataset, class_names, field_name="segmentations"
         for obj in sample[field_name]["detections"]:
             label = obj.label
 
-            if label not in class_names:
+            if class_names is not None and label not in class_names:
                 continue
+
+            index += 1
 
             _box = obj.bounding_box  # [x, y, w, h] / s
             x, y = round(_box[0] * img_w), round(_box[1] * img_h)
@@ -45,23 +47,25 @@ def from_coco_instance(out_dir, dataset, class_names, field_name="segmentations"
             w, h = min(mask_w, img_w - x), min(mask_h, img_h - y)
 
             mask = obj.mask.astype("uint8") * 255
-            mask = np.stack((mask, mask, mask), axis=-1)
 
             patch_mask = mask[:h, :w]
             patch = img[y:y + h, x:x + w]
 
-            _db[label].append({
-                "image": img_file,
-                "mask": mask_file,
-                "box": [x, y, w, h],
-                "whole": (img_h, img_w),
-            })
-            img_file = f"data/{label}/{prefix}_{len(_db):06d}.jpg"
-            mask_file = f"data/{label}/{prefix}_{len(_db):06d}.png"
+            file_stem = f"{prefix}_{index:06d}"
+            patch_file = f"data/{label}/{file_stem}.jpg"
+            mask_file = f"data/{label}/{file_stem}.png"
 
-            cv.imwrite(str(out_dir / img_file), patch)
+            _db[file_stem] = {
+                "label": label,
+                "box": [x, y, w, h],
+                "imgsz": [img_h, img_w],
+                "patch": patch_file,
+                "mask": mask_file,
+            }
+
+            cv.imwrite(str(out_dir / patch_file), patch)
             cv.imwrite(str(out_dir / mask_file), patch_mask)
 
     with open(out_dir / "db.json", "w") as f:
-        json.dump(_db, f)
+        json.dump(_db, f, indent=4)
     return str(out_dir)
