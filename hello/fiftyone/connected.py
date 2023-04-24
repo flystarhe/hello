2dfa95995c4174ce0252a71bb2af5466af75d47b
hejian* <flystarhe@qq.com>
2023-04-24 18:40:55 +0800

    update hello/fiftyone/connected.py

--- hello/fiftyone/connected.py
+++ hello/fiftyone/connected.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
 import cv2 as cv
-from skimage.measure import label
+from skimage.measure import label, regionprops
 from tqdm import tqdm
 
 
-def count_areas(image_dir, pattern="*.png"):
+def count_areas(image_dir, pattern="*.png", limit=None):
     """Count connected regions of an integer array.
 
     https://scikit-image.org/docs/dev/api/skimage.measure.html#skimage.measure.label
@@ -22,7 +22,12 @@ def count_areas(image_dir, pattern="*.png"):
     for f in tqdm(files):
         x = cv.imread(f, 0)
         labels = label(x, connectivity=x.ndim)
-        counts.append((f, labels.max()))
+        if limit is None:
+            n = labels.max()
+        else:
+            props = regionprops(labels)
+            n = len([1 for r in props if r.num_pixels > limit])
+        counts.append((f, n))
 
     print(f"{len(counts)=}, {sum([v[1] for v in counts])=}")
     return counts
