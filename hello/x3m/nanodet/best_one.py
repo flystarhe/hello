cbcb5fd87caa72317c56432ba2ab61f377d0a62b
hejian* <flystarhe@qq.com>
2023-06-08 09:28:42 +0800

    update hello/x3m/nanodet/best_one.py - test_notebook()

--- hello/x3m/nanodet/best_one.py
+++ hello/x3m/nanodet/best_one.py
@@ -77,7 +77,7 @@ def post_process(outputs, input_shape, reg_max=7):
     return bbox, score, cls_id
 
 
-def show_bbox(bgr_image, input_shape, bboxes, scores, cls_ids, cls_names=None):
+def show_bbox(bgr_image, infer_scale, bboxes, scores, cls_ids, cls_names=None):
     import cv2 as cv
     from IPython.display import display
     from PIL import Image
@@ -85,8 +85,8 @@ def show_bbox(bgr_image, input_shape, bboxes, scores, cls_ids, cls_names=None):
         bgr_image = cv.imread(bgr_image, 1)
 
     f = 1.0
-    if bgr_image.shape[:2] != tuple(input_shape):
-        f = bgr_image.shape[0] / input_shape[0]
+    if bgr_image.shape[:2] != tuple(infer_scale):
+        f = bgr_image.shape[0] / infer_scale[1]  # scale(w, h)
 
     for bbox, score, cls_id in zip(bboxes, scores, cls_ids):
         cls_name = cls_names[cls_id] if cls_names else f"id:{cls_id}"
@@ -98,7 +98,41 @@ def show_bbox(bgr_image, input_shape, bboxes, scores, cls_ids, cls_names=None):
     display(Image.fromarray(rgb_image, "RGB"))
 
 
-def test():
-    output = np.random.rand(1, 3, 5, 34) * 1
-    ret = post_process([output], [4])
+def test_post_process():
+    output = np.random.rand(1, 3, 5, 34) * 1.0
+    ret = post_process([output], (12, 20))
     return ret
+
+
+def test_notebook():
+    """X3M Ai Toolchain Docker Container.
+
+        docker pull openexplorer/ai_toolchain_centos_7_xj3:v2.4.2
+        docker run -it --rm -p 7000:9000 --ipc=host -v $(pwd):/workspace openexplorer/ai_toolchain_centos_7_xj3:v2.4.2 bash
+        nohup jupyter notebook --ip='*' --port=9000 --notebook-dir='/workspace' --NotebookApp.token='hi' --no-browser --allow-root > /workspace/nohup.out 2>&1 &
+        # localhost:7000/tree?token=hi
+    """
+    import cv2 as cv
+    from horizon_tc_ui import HB_ONNXRuntime
+
+    infer_scale = (640, 360)  # (w/3, h/3)
+    input_shape = (384, 640)  # divisible by 64
+    image_file = "data/20230309_163529_i000246.jpg"
+    model_file = "test/model_output/nanodet_384x640_x3_bgr_quantized_model.onnx"
+
+    sess = HB_ONNXRuntime(model_file=model_file)
+    print(f"{sess.input_names}, {sess.output_names}, {sess.layout}")
+
+    image = cv.imread(image_file, 1)  # bgr
+    image = cv.resize(image, infer_scale)  # (w, h)
+    pad_image = np.full(input_shape + (3,), (114, 114, 114), dtype="uint8")
+    pad_image[:360, :640, :] = image
+
+    image_data = pad_image[np.newaxis, ...]  # (1, h, w, c)
+    input_name, output_names = sess.input_names[0], sess.output_names
+    outputs = sess.run(output_names, {input_name: image_data}, input_offset=128)
+    bbox, score, cls_id = post_process(outputs, input_shape, reg_max=7)
+
+    show_bbox(image_file, infer_scale, [bbox], [score], [cls_id], cls_names=["charging station", "other object"])
+
+    return bbox, score, cls_id
